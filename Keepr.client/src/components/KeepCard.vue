71abb360c2ffd190ed43820814a34f0672f68089
Tyler Truman <trucotyler@gmail.com>
2022-09-26 15:39:10 -0600

    progress

--- Keepr.client/src/components/KeepCard.vue
+++ Keepr.client/src/components/KeepCard.vue
@@ -1,30 +1,38 @@
 <template>
-<figure class="selectable">
+<figure class="selectable" @click="setActive()">
   <img class="keep-img rounded" :src="keep.img" alt="">
   <h6 class="keep-name text-light">{{keep.name}} </h6>
     <img class="profile-img rounded-circle" :src="keep.creator.picture" height="30">
 </figure>
+<KeepModal/>
 </template>
 
 <script>
+import { Modal } from 'bootstrap';
+import { keepsService } from '../services/KeepsService';
+import { logger } from '../utils/Logger';
+import KeepModal from './KeepModal.vue';
 export default {
-  props: {
-    keep: {
-      type: Object,
-      required: true
-    }
-  },
-setup(props) {
-  return {
-    async setActive() {
-      try {
-        
-      } catch (error) {
-        
-      }
-    }
-  };
-},
+    props: {
+        keep: {
+            type: Object,
+            required: true
+        }
+    },
+    setup(props) {
+        return {
+            async setActive() {
+                try {
+                  Modal.getOrCreateInstance(document.getElementById("keepModal")).toggle();
+                  await keepsService.getOne(props.keep.id)
+                }
+                catch (error) {
+                  logger.error(error);
+                }
+            }
+        };
+    },
+    components: { KeepModal }
 };
 </script>
 
---
+++ Keepr.client/src/components/KeepModal.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="modal fade" id="keepModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+        <div class="modal-header ">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body ">
+          <div class="row">
+            <div class="col-12 col-md-6">
+              <img class="img-fluid rounded" :src="keep?.img" alt="">
+            </div>
+            <div class="col-12 col-md-6">
+              <div class="text-center">
+                <span class="mx-2">Views: {{keep?.views}}</span>
+                <span class="mx-2">Saves: {{keep?.kept}}</span>
+                <span class="mx-2">Shares: {{keep?.shares}}</span>
+                <h1 class="pt-3">{{keep?.name}}</h1>
+              <div class="pt-2">
+                {{keep?.description}}
+              </div>
+              <hr>
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-center align-content-center">
+            <button class="btn btn-info col-4">Add To Vault</button>
+            <span class="selectable text-danger col-1 align-self-center px-3">X</span>
+            <img class="rounded col-2 p-0 selectable" :src="keep.creator?.picture" alt="Keep Creator Info" height="50" >
+            <span class="col-5 align-self-center">{{keep.creator?.name}}</span>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity';
+import { AppState } from '../AppState';
+
+export default {
+setup() {
+  return {
+    keep: computed(() => AppState.activeKeep)
+  };
+},
+};
+</script>
+
+<style scoped>
+/* .modal {
+  height: 100vh !important;
+} */
+</style>
